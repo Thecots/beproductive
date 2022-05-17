a97f0ed3941050236e76539a9851eb2bbe042fcb
thecots cots <thecots22@gmail.com>
2022-05-17 11:56:20 +0200

    new goal

--- server/public/js/global.js
+++ server/public/js/global.js
@@ -3,7 +3,7 @@ window.onload = function() {
   loadScreen(true);
   menuTareas()
   /* changeWeb(1,{year: date.getFullYear()}) */
-  changeWeb(3,{year: 2022, month: 11, day: 30})
+  changeWeb(1,{year: 2022})
   setInterval(timer, 100)
   loadScreen(false)
 };
@@ -12,7 +12,8 @@ const month = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto'
 const day = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sábado']
 const menu = document.querySelector('menu');
 const currentMoment = document.querySelector('.currentMoment');
-
+const main = document.querySelector('section.main');
+const formSection = document.querySelector('section.form')
 
 function timer(){
   const date = new Date();
@@ -27,6 +28,9 @@ function changeWeb(e,data){
   menu.children[1].setAttribute('onclick',`changeWeb(2,{year: ${date.getFullYear()}, month: ${date.getMonth()}})`)
   menu.children[2].setAttribute('onclick',`changeWeb(3,{year: ${date.getFullYear()}, month: ${date.getMonth()}, day: ${date.getDate()}})`)
 
+  main.style.display = 'grid'
+  formSection.style.display = 'none'
+
   let active = Object.values(menu.children).findIndex(n => n.classList[0] == 'active')
   menu.children[active].classList.remove('active')
   menu.children[e-1].classList.add('active')
@@ -35,7 +39,7 @@ function changeWeb(e,data){
   
   /* anual  */
   if(e == 1){
-    createbtn.setAttribute('onclick','newGoal(1)')
+    createbtn.setAttribute('onclick',`newGoal(1,{year: ${data.year}})`)
     currentMoment.innerHTML = `
       <i onclick="changeWeb(1,{year: ${data.year-1}})" class="fa-solid fa-angle-left"></i>
       <h1>${data.year}</h1>
@@ -57,7 +61,7 @@ function changeWeb(e,data){
 
   /* menusal */
   if(e == 2){
-    createbtn.setAttribute('onclick','newGoal(2)')
+    createbtn.setAttribute('onclick',`newGoal(2,{year: ${data.year}, month: ${data.month}})`)
     currentMoment.innerHTML = `
       <i onclick="changeWeb(2,{year: ${data.month == 0 ? data.year-1 : data.year}, month: ${data.month == 0 ? 11 : data.month-1}} )" class="fa-solid fa-angle-left"></i>
       <h1>${month[data.month]} del ${data.year}</h1>
@@ -68,7 +72,7 @@ function changeWeb(e,data){
 
   /* diario */
   if(e == 3){
-    createbtn.setAttribute('onclick','newGoal(3)')
+    createbtn.setAttribute('onclick',`newGoal(3,{year: ${data.year}, month: ${data.month}, day: ${data.day}})`)
     dim = new Date(data.year, data.month+1, 0).getDate();
     dbim = new Date((data.month == 0 ? data.year-1 : data.year), (data.month == 0 ? 12 : data.month), 0).getDate();
     
@@ -92,6 +96,134 @@ function menuTareas(){
     }
   })
 }
+
+function newGoal(e, data){
+  main.style.display = 'none'
+  formSection.style.display = 'grid'
+
+  /* anual  */
+  if(e == 1){
+    let template = `
+      <div class="title"><h1>Tarea para el ${data.year}</h1></div>
+      <form>
+        <section>
+          <div class="text-field">
+            <input id="ftitle" maxlength="15" type="text" required autofocus> 
+            <label class="label">Tarea</label>
+          </div>
+          <div class="text-field">
+            <textarea id="fdescription" maxlength="15" type="text"></textarea>
+            <label class="label">Descrición <small style="font-size: 10px;">(opcional)</small></label>
+          </div>
+          <input hidden id="goaltype" value="${e}">
+          <input hidden id="goalyear" value="${data.year}">
+          <input hidden id="goalmonth" value="1">
+          <input hidden id="goalday" value="1">
+          <button type="submit">
+            <i class="fa-solid fa-circle-plus"></i>
+          </button>
+        </section>
+      </form>
+    `
+    formSection.innerHTML = template;
+  }
+
+  /* menusal */
+  if(e == 2){
+    let template = `
+    <div class="title"><h1>Tarea para ${month[data.month]} del ${data.year}</h1></div>
+    <form>
+      <section>
+        <div class="text-field">
+          <input id="ftitle" maxlength="15" type="text" required autofocus> 
+          <label class="label">Tarea</label>
+        </div>
+        <div class="text-field">
+          <textarea id="fdescription" maxlength="15" type="text"></textarea>
+          <label class="label">Descrición <small style="font-size: 10px;">(opcional)</small></label>
+        </div>
+        <input hidden id="goaltype" value="${e}">
+        <input hidden id="goalyear" value="${data.year}">
+        <input hidden id="goalmonth" value="${data.month}">
+        <input hidden id="goalday" value="1">
+        <button type="submit">
+          <i class="fa-solid fa-circle-plus"></i>
+        </button>
+      </section>
+    </form>
+  `
+  formSection.innerHTML = template;
+}
+
+  /* diario */
+  if(e == 3){
+    let date = new Date(data.year, data.month, data.day)
+    
+    let template = `
+    <div class="title"><h1>Tarea para el ${day[date.getDay()]} ${data.day} de ${month[data.month]} del ${data.year}</h1></div>
+    <form>
+      <section>
+        <div class="text-field">
+          <input id="ftitle" maxlength="15" type="text" required autofocus> 
+          <label class="label">Tarea</label>
+        </div>
+        <div class="text-field">
+          <textarea id="fdescription" maxlength="15" type="text"></textarea>
+          <label class="label">Descrición <small style="font-size: 10px;">(opcional)</small></label>
+        </div>
+        <input hidden id="goaltype" value="${e}">
+        <input hidden id="goalyear" value="${data.year}">
+        <input hidden id="goalmonth" value="${data.month}">
+        <input hidden id="goalday" value="${data.day}">
+        <button type="submit">
+          <i class="fa-solid fa-circle-plus"></i>
+        </button>
+      </section>
+    </form>
+  `
+  formSection.innerHTML = template;
+  }
+
+
+  const form = document.querySelector('form')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    let type = document.querySelector('#goaltype').value;
+    let title = document.querySelector('#ftitle').value;
+    let description = document.querySelector('#fdescription');
+
+    let year = document.querySelector('#goalyear').value
+    let month = document.querySelector('#goalmonth').value
+    let day = document.querySelector('#goalday').value
+    
+
+    fetch('/savegoal', {
+      method: 'POST',
+      body: JSON.stringify({
+        type,
+        title,
+        description: description.value,
+        data: {year, month, day}
+      }),
+      headers:{'Content-Type': 'application/json'}
+    })
+    .then(r => r.json())
+    .then(r => {
+      if(r.type == 1){
+        changeWeb(1,{year: r.data.year})
+      }
+      if(r.type == 2){
+        changeWeb(2,{year: r.data.year, month: r.data.month})
+      }
+      if(r.type == 3){
+        changeWeb(3,{year: r.data.year, month: r.data.month, day: r.data.day})
+      }
+    })
+       
+  })
+}
+
 
 function loadScreen(e){
   const loader = document.querySelector('#loadScreen')
--- server/routes/main.routes.js
+++ server/routes/main.routes.js
@@ -16,7 +16,6 @@ router.post('/getTareas', [session, getUserArea], async(req, res) => {
   const mensual = await sql(`SELECT count(*) as count FROM goals WHERE MONTH(created) = ${date.getFullYear()} AND checked = 0 AND user = ${req.session.id}`)
   const diario = await sql(`SELECT count(*) as count FROM goals WHERE DAY(created) = ${date.getFullYear()} AND checked = 0 AND user = ${req.session.id}`)
   
-  console.log(anual.res[0].count);
   res.json({
     ok: true,
     anual: diario.res[0].count,
@@ -29,6 +28,16 @@ router.post('/goals/year' , [session, getUserArea], async(req, res) => {
   const anual = await sql(`SELECT count(*) as count FROM goals WHERE YEAR(created) = ${req.body.year} AND class = 0 AND user = ${req.session.id}`)
   res.json({ok:true, anual: anual.res})
 })
+
+router.post('/savegoal' , [session, getUserArea], async(req, res) => {
+  let e = req.body
+  let goal = await sql(`INSERT INTO goals VALUES (null,'${e.title}','${e.description}', 0, DATE '${e.data.year}/${e.data.month}/${e.data.day}', ${req.session.id}, ${e.type})`)
+  console.log(goal.ok);
+  res.json({ok:true, type: e.type, data: e.data})
+})
+
 router.use(require('./login.routes'))
+
+
 
 module.exports = router;
